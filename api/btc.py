98d04a8f0e71d1b6effe5b58f22a6f9daf6cb59d
Admin <coder188@applex.io>
2018-07-30 20:35:26 +0800

    增加ip

--- api/btc.py
+++ api/btc.py
@@ -1,12 +1,12 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
 class Btc(object):
-	rpc_ip = '127.0.0.1'
-    def __init__(self,rpc_port,rpc_user,rpc_password):
+    def __init__(self,rpc_ip,rpc_port,rpc_user,rpc_password):
+    	self.rpc_ip = '127.0.0.1'
         self.rpc_user = rpc_user
         self.rpc_password = rpc_password
         self.rpc_port = rpc_port
-	    self.rpc_connection = AuthServiceProxy("http://%s:%s@%s:%s"%(self.rpc_user,self.rpc_password,rpc_ip,self.rpc_port))
+	    self.rpc_connection = AuthServiceProxy("http://%s:%s@%s:%s"%(self.rpc_user,self.rpc_password,self.rpc_ip,self.rpc_port))
 
 
     def getnewaddress(self):
