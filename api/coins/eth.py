a0be759a860e2f62c1eb92396e8a6c31d991ba95
Admin <coder188@applex.io>
2018-08-24 17:22:52 +0800

    eth 转账bug修改

--- api/coins/eth.py
+++ api/coins/eth.py
@@ -50,7 +50,9 @@ class Eth(object):
         if passphrase and from_ and to:
             #transaction = {from:from_, to: to, value: self.w3.toWei(value, "ether")}
             # tx = {from:from_,to:to,value:self.w3.toWei(value,"ether")}
-            tx = {from:from_,to:to,value:self.w3.toWei(amount,'ether')}
+            tx = { 'from': from_,
+                    'to': to,
+                    'value':self.w3.toWei(amount,'ether')}
             txid = self.w3.personal.sendTransaction(tx, passphrase)
             if txid:
                 return {'fromaddress':from_,'toaddress':to,'category':'send','time':datetime.now(),'txid':txid,'amount':amount}
