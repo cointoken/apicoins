c6a0ebcb33fc01fb57232cf1cc8657c2a0064d87
Admin <coder188@applex.io>
2018-09-05 09:51:08 +0800

    mysql修改

--- api/config.py
+++ api/config.py
@@ -31,4 +31,4 @@ LOGGING_CONFIG = {
     }
 }
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:admin@localhost/exchange'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:toApx08c@localhost/exchange'
