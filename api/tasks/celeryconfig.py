570f83f63e0212ed0ef9b1ea1b91ea182eab23f6
Admin <coder188@applex.io>
2018-09-07 16:09:01 +0800

    增加task

--- api/tasks/celeryconfig.py
+++ api/tasks/celeryconfig.py
@@ -6,6 +6,7 @@ BROKER_URL = 'redis://127.0.0.1:6379'
 CELERY_TIMEZONE='Asia/Shanghai'
 
 CELERY_IMPORTS = (
+    'tasks.task1',
     'tasks.task2'
 )
 #'tasks.task1'
@@ -15,13 +16,12 @@ CELERYBEAT_SCHEDULE = {
         'task': 'tasks.task2.get_ethereum_addresses',
         'schedule': timedelta(minutes=5),       # 每 5 分钟执行一次
         'args': ()
+    },
+    'add-every-20-minutes': {
+         'task': 'tasks.task1.import_deposits',
+         'schedule': timedelta(minutes=20),       # 每 20 分钟执行一次
+         'args': ()
     }
-    # },
-    # 'add-every-20-minutes': {
-    #      'task': 'tasks.task1.import_deposits',
-    #      'schedule': timedelta(minutes=20),       # 每 20 分钟执行一次
-    #      'args': ()
-    # },
     # 'multiply-at-12-time': {
     #     'task': 'tasks.task.autotransfer',
     #     'schedule': crontab(hour=12, minute=00),   # 每天中午 12点 00分执行一次
