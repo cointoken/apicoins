ba9b5a91e69b2ce043ab11a406b691f410eb983a
Admin <coder188@applex.io>
2018-09-11 18:52:46 +0800

    修改时间

--- api/tests/test_arrays.py
+++ api/tests/test_arrays.py
@@ -1,1 +1,10 @@
 import unittest
+from datetime import datetime,timedelta
+
+
+def now_time(hours):
+    return (datetime.now()-timedelta(hours=hours)).strftime('%Y-%m-%d')
+
+if __name__=='__main__':
+    print(now_time(8))
+    print(now_time(0))
