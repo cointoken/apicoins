0ccc4108bb404e0fc3349538d3562a3774da4d69
Admin <coder188@applex.io>
2018-09-11 18:55:48 +0800

    修改时间1

--- api/tasks/task1.py
+++ api/tasks/task1.py
@@ -9,13 +9,13 @@ from dbs.crud import CRUD
 import config
 
 
-def now_time():
-    return datetime.now().strftime('%Y-%m-%d')
+def now_time(hours):
+    return (datetime.now()-datetime.timedelta(hours=hours)).strftime('%Y-%m-%d')
 
 
 @worker.task
 def import_deposits():
-    deposit_url = 'http://47.52.209.94:3030/admin/success_deposits?begin_time={0}&end_time={1}'.format(now_time(),now_time())
+    deposit_url = 'http://47.52.209.94:3030/admin/success_deposits?begin_time={0}&end_time={1}'.format(now_time(24),now_time(0))
     engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
     crud = CRUD(engine)
 
@@ -29,7 +29,7 @@ def import_deposits():
                 for d in datas:
                     fee = 0 if d['fee'] is None else float(d['fee'])
                     amount = 0 if d['amount'] is None else float(d['amount'])
-                    
+
                     de = Deposits(d['id'],d['currency'],d['email'],d['phone_number'],amount,fee,d['fund_uid'],datetime.strptime(d['created_at'],'%Y-%m-%d %H:%M:%S'))
                     crud.deposits_insert(de)
         except:
