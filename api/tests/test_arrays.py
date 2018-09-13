bcdcd647aa6ad98d75e7f10819f63a39c849b0b4
Admin <coder188@applex.io>
2018-09-13 15:47:23 +0800

    eth_etc

--- api/tests/test_arrays.py
+++ api/tests/test_arrays.py
@@ -1,18 +1,17 @@
-import unittest
-from datetime import datetime,timedelta
-from coins.btc import Btc
-from coins.eth import Eth
+# import unittest
+# from datetime import datetime,timedelta
+# from coins.btc import Btc
+# from coins.eth import Eth
 
-def now_time(hours):
-    return (datetime.now()-timedelta(hours=hours)).strftime('%Y-%m-%d')
+# # def now_time(hours):
+# #     return (datetime.now()-timedelta(hours=hours)).strftime('%Y-%m-%d')
 
-def test_btc():
-    btc = Btc(8332,'apx','DEOXMEIO943JKDJFIE3312DFKIEOK')
-    btc_accounts = btc.listaccounts()
-    for key,value in btc_accounts.items():
-        amount = btc_accounts[key]
-        print(amount)
+# # def test_btc():
+# #     btc = Btc(8332,'apx','DEOXMEIO943JKDJFIE3312DFKIEOK')
+# #     btc_accounts = btc.listaccounts()
+# #     for key,value in btc_accounts.items():
+# #         amount = btc_accounts[key]
+# #         print(amount)
 
 if __name__=='__main__':
-    print(now_time(48))
-    print(now_time(0))
+    pass
